620738f45409ae333b40dbef7a28714ec1140a7b
Jesse/Jebem <jesse.kirjavainen@pp.inet.fi>
2019-11-16 12:07:46 +0200

    Begone default react app readme

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>ViewFinland</h1>
+        <p>
+          You know you want to.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
